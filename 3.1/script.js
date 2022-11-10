5a52ca85dae1d679068f66f2630ce8dab321ba7e
Mazurkevichvs <mazurkevichvs@gmail.com>
2022-11-10 13:07:09 +0100

    [task_1]: Add Object.defineProperty and if() changes

--- 3.1/script.js
+++ 3.1/script.js
@@ -1,13 +1,15 @@
-Array.prototype.customFilter = function (func, obj)  {
-    if (typeof(func) === 'function') {
-        const filtered = []
-        for (let i = 0; i < this.length; i++) {
-            if (func.call(obj, this[i], i , this)) {       
-                filtered.push(this[i])
+Object.defineProperty(Array.prototype, 'customFilter', {
+    value: function (func, obj={}) {
+        if (typeof(func) === 'function' && typeof(obj) === 'object' && !Array.isArray(obj)) {
+            const filtered = []
+            for (let i = 0; i < this.length; i++) {
+                if (func.call(obj, this[i], i , this)) {       
+                    filtered.push(this[i])
+                }
             }
-        }
-        return filtered
-    } else {
-        throw new Error()
-    }   
-}
+            return filtered
+        } else {
+            throw new Error()
+        }   
+    }
+})   
